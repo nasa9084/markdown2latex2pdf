3625d2de7c4ccc4256de4c5b6d4a8c809b3dd60d
nasa9084 <nasa9084@live.jp>
2017-05-09 14:31:54 +0900

    add: can use template with docker container

--- docker/compile.sh
+++ docker/compile.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cp ../workdir/document.md ./
-./md2pdf.sh document.md
+cp ../workdir/* ./
+if [ -e ../workdir/template.tex ]; then
+   ./md2pdf.sh -t template.tex document.md
+else
+    ./md2pdf.sh document.md
+fi
 cp document.md.pdf ../workdir/document.pdf
